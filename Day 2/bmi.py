16714c4ca09198e902547bc75ee891e06b9f5467
Mohammad Tareq <tareqatwork@outlook.com>
2023-07-13 12:51:29 +0600

    bmi

---
+++ Day 2/bmi.py
@@ -0,0 +1,8 @@
+height = input('Please enter your height in m:\n')
+weight = input('Please enter your weight in kg:\n')
+
+bmi = float(weight) / float(height) ** 2
+
+formatted_bmi = round(bmi , 2)
+
+print(f'Your bmi is {formatted_bmi}')
