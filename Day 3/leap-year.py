684eab5df9a69856bb0c89a4da3ead209f68f190
Mohammad Tareq <tareqatwork@outlook.com>
2023-07-13 15:12:09 +0600

    update leap year

--- Day 3/leap-year.py
+++ Day 3/leap-year.py
@@ -1,9 +1,19 @@
 input = input('Enter year: ')
 year = int(input)
 
-if year%4 == 0 and year%100 != 0:
-    print(f'{year} is a leap Year')
-elif year%400 == 0:
-    print(f'{year} is a leap Year')
+# if year%4 == 0 and year%100 != 0:
+#     print(f'{year} is a leap Year')
+# elif year%400 == 0:
+#     print(f'{year} is a leap Year')
+# else:
+#     print(f'{year} is not a leap Year')
+
+if year%4 == 0 :
+    if year%100 != 0:
+        print(f'{year} is a leap Year')
+    elif year%400 == 0:
+        print(f'{year} is a leap Year')
+    else:
+        print(f'{year} is not a leap Year')
 else:
     print(f'{year} is not a leap Year')
