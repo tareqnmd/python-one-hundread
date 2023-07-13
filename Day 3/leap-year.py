1a4c97b4294b3ca7ca672b7fa3a9134def174c18
Mohammad Tareq <tareqatwork@outlook.com>
2023-07-13 15:01:04 +0600

    leap year added

---
+++ Day 3/leap-year.py
@@ -0,0 +1,9 @@
+input = input('Enter year: ')
+year = int(input)
+
+if year%4 == 0 and year%100 != 0:
+    print(f'{year} is a leap Year')
+elif year%400 == 0:
+    print(f'{year} is a leap Year')
+else:
+    print(f'{year} is not a leap Year')
