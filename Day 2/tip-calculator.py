d157019711e30c7c2575bfb72e38e87ee90a2a1b
Mohammad Tareq <tareqatwork@outlook.com>
2023-07-13 14:26:58 +0600

    update tip calc

--- Day 2/tip-calculator.py
+++ Day 2/tip-calculator.py
@@ -8,7 +8,9 @@ tip_percentage_float = float(tip_percentage)
 total_bill_with_tip = total_bill_float + ( total_bill_float * (tip_percentage_float / 100) )
 total_people_int = int(total_people)
 
-splitted_bill = round( total_bill_with_tip / total_people_int , 2)
+splitted_bill =  total_bill_with_tip / total_people_int
+# rounded_splitted_bill =round( total_bill_with_tip / total_people_int , 2)
+rounded_splitted_bill = "{:.2f}".format(splitted_bill)
 
-each_person_pay=f'Each person should pay: {splitted_bill}'
+each_person_pay=f'Each person should pay: {rounded_splitted_bill}'
 print(each_person_pay)
