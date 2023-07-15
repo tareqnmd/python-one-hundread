34b78761d05ef2d6214ed12d7bf47f8936759bae
Mohammad Tareq <tareqatwork@outlook.com>
2023-07-15 18:51:14 +0600

    rock-paper-scissors update

---
+++ Day 4/rock-paper-scissors.py
@@ -0,0 +1,51 @@
+import random
+
+rock = """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+
+paper = """
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+"""
+
+scissors = """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+
+
+def chooseAsci(type):
+    if type == 0:
+        return rock
+    elif type == 1:
+        return paper
+    elif type == 2:
+        return scissors
+
+
+person_choose_input = input(
+    "What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissor. "
+)
+
+person_choose = int(person_choose_input)
+print(f"You Choose {chooseAsci(person_choose)}")
+computer_choose = random.randint(0, 2)
+print(f"Computer Choose {chooseAsci(computer_choose)}")
+
+
+if person_choose == computer_choose:
+    print("Match Draw")
