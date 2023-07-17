84dde2d79970f5cd0e8c0b70a0e6743ea7c820ed
Mohammad Tareq <tareqatwork@outlook.com>
2023-07-17 11:22:34 +0600

    update hangman

--- Day 7/hangman.py
+++ Day 7/hangman.py
@@ -1,23 +1,99 @@
 import random
 
+stages = [
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========""",
+    """
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+""",
+]
+
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-print(chosen_word)
 chosen_word_length = len(chosen_word)
+lives_remaining = 6
+print(f"lives Remaining : {lives_remaining}")
 
 display = []
 for _ in range(chosen_word_length):
     display += "_"
 
-for _ in range(chosen_word_length):
+end_of_game = False
+
+while not end_of_game:
     guess = input("Guess a letter: ").lower()
+
     for position in range(chosen_word_length):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
-    print(display)
 
-if "_" not in (display):
-    print("You Win")
-else:
-    print("You Lose")
+    if guess not in chosen_word:
+        lives_remaining -= 1
+        print(f"{stages[lives_remaining]}")
+        if lives_remaining == 0:
+            end_of_game = True
+            print("You Lose")
+
+    print(f"{' '.join(display)}")
+
+    if "_" not in (display):
+        end_of_game = True
+        print("You Win")
