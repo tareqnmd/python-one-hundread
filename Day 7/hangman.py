e99c1fb234a39f7c630401d071b203ca69d41e0f
Mohammad Tareq <tareqatwork@outlook.com>
2023-07-17 11:47:30 +0600

    hangman update

--- Day 7/hangman.py
+++ Day 7/hangman.py
@@ -1,75 +1,14 @@
 import random
+import os
 
-stages = [
-    """
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========""",
-    """
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-""",
-    """
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-""",
-]
+from hangman_arts import logo, stages
+from hangman_words import word_list
 
-word_list = ["aardvark", "baboon", "camel"]
+print(f"{logo}\n")
 chosen_word = random.choice(word_list)
 chosen_word_length = len(chosen_word)
 lives_remaining = 6
-print(f"lives Remaining : {lives_remaining}")
+print(f"lives Remaining : {chosen_word,lives_remaining}")
 
 display = []
 for _ in range(chosen_word_length):
@@ -79,21 +18,28 @@ end_of_game = False
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
-
-    for position in range(chosen_word_length):
-        letter = chosen_word[position]
-        if letter == guess:
-            display[position] = letter
-
-    print(f"{' '.join(display)}")
+    os.system('clear')
+    if guess in display:
+        print(f"You already guessed {guess}")
+    else:
+        for position in range(chosen_word_length):
+            letter = chosen_word[position]
+            if letter == guess:
+                display[position] = letter
 
     if guess not in chosen_word:
         lives_remaining -= 1
-        print(f"{stages[lives_remaining]}")
+        print(
+            f"you guessed {guess}, that's not in the word. you lose a life. Remaining life : {lives_remaining}"
+        )
         if lives_remaining == 0:
             end_of_game = True
             print("You Lose")
 
+    print(f"{' '.join(display)}")
+
     if "_" not in (display):
         end_of_game = True
         print("You Win")
+
+    print(f"{stages[lives_remaining]}")
