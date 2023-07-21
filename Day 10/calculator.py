b241babc6a1128149442b2af2a03ee3065e4ad1f
Mohammad Tareq <tareqatwork@outlook.com>
2023-07-21 12:13:36 +0600

    calc

--- Day 10/calculator.py
+++ Day 10/calculator.py
@@ -12,27 +12,35 @@ def subtract(num1, num2):
 
 
 def multiply(num1, num2):
-    return num1 + num2
+    return num1 * num2
 
 
 def divide(num1, num2):
-    return num1 + num2
+    return num1 / num2
 
 
 operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
 
 
-calc_continue = True
-
-while calc_continue:
+def calculator():
+    calc_continue = True
     num1 = float(input("What's the number? :"))
-    for symbol in operations:
-        print(symbol)
-    operation = input("Pick an operation: ")
-    num2 = float(input("What's the other number? :"))
-    calc = operations[operation]
-    result = calc(num1, num2)
-    print(f"{num1} {operation} {num2} = {result}")
-    next = input(
-        f"Type 'y' to continue calculating with {result} or type 'n' to start a new calculation: "
-    )
+    while calc_continue:
+        for symbol in operations:
+            print(symbol)
+        operation = input("Pick an operation: ")
+        num2 = float(input("What's the other number? :"))
+        calc = operations[operation]
+        result = calc(num1, num2)
+        print(f"{num1} {operation} {num2} = {result}")
+        next = input(
+            f"Type 'y' to continue calculating with {result} or type 'n' to start a new calculation: "
+        )
+        if next == "y":
+            num1 = result
+        else:
+            calc_continue = False
+            calculator()
+
+
+calculator()
