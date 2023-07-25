c55d56d9c0dda8b99b97e03806d44c8fff4f546a
Tareq <tareqnmd@gmail.com>
2023-07-25 10:29:49 +0600

    update guess

--- Day 12/guess_number.py
+++ Day 12/guess_number.py
@@ -2,15 +2,24 @@ from random import randint
 
 from art import logo
 
+HARD_ATTEMPTS = 5
+EASY_ATTEMPTS = 10
+
+
+def check_difficulty(type):
+    if type == "hard":
+        return HARD_ATTEMPTS
+    else:
+        return EASY_ATTEMPTS
+
+
 print(logo)
 print("Welcome to the Number Guessing Game!")
 print("I am thinking of a number between 1 and 100")
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+attempts = check_difficulty(difficulty)
 selected_number = randint(1, 100)
-if difficulty == "hard":
-    attempts = 5
-else:
-    attempts = 10
+
 
 while attempts > 0:
     print(f"You have {attempts} attempts remaining to guess the number")
