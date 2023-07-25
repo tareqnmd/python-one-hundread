378d91826cb100ca87bbdd19e3bc1ad511a3a55a
Tareq <tareqnmd@gmail.com>
2023-07-25 10:45:58 +0600

    guessed number

--- Day 12/guess_number.py
+++ Day 12/guess_number.py
@@ -6,6 +6,13 @@ HARD_ATTEMPTS = 5
 EASY_ATTEMPTS = 10
 
 
+print(logo)
+print("Welcome to the Number Guessing Game!")
+print("I am thinking of a number between 1 and 100")
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+selected_number = randint(1, 100)
+
+
 def check_difficulty(type):
     if type == "hard":
         return HARD_ATTEMPTS
@@ -13,26 +20,29 @@ def check_difficulty(type):
         return EASY_ATTEMPTS
 
 
-print(logo)
-print("Welcome to the Number Guessing Game!")
-print("I am thinking of a number between 1 and 100")
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
-attempts = check_difficulty(difficulty)
-selected_number = randint(1, 100)
-
-
-while attempts > 0:
-    print(f"You have {attempts} attempts remaining to guess the number")
+def check_number():
     guessed_number = int(input("Make a guess: "))
     if selected_number == guessed_number:
         print(f"You got it! The answer was {guessed_number}")
-        break
+        return 1
     elif selected_number > guessed_number:
         print("Too Low.")
+        return 0
     elif selected_number < guessed_number:
         print("Too High.")
+        return 0
+
+
+attempts = check_difficulty(difficulty)
+
+while attempts > 0:
+    print(f"You have {attempts} attempts remaining to guess the number")
     attempts -= 1
-    if attempts == 0:
-        print("You are ran out of guesses, you lose.")
+    result = check_number()
+    if result == 1:
+        break
     else:
-        print("Guess Again.")
+        if attempts == 0:
+            print("You are ran out of guesses, you lose.")
+        else:
+            print("Guess Again.")
