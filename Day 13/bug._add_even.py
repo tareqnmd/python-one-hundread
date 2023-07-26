789981eb14839a6df0632fa6c5f54a1808e583dd
Tareq <tareqnmd@gmail.com>
2023-07-26 16:08:14 +0600

    bug 13

---
+++ Day 13/bug._add_even.py
@@ -0,0 +1,18 @@
+# input = input("Enter your number: ")
+
+# input_int = int(input)
+
+# if input_int % 2 = 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+
+input = input("Enter your number: ")
+
+input_int = int(input)
+
+if input_int % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
