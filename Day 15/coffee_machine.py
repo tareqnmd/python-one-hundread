9bb47cc8827681c9b56cfc5112981851fb6c68df
Tareq <tareqnmd@gmail.com>
2023-07-27 10:38:46 +0600

    report off

--- Day 15/coffee_machine.py
+++ Day 15/coffee_machine.py
@@ -1,4 +1,22 @@
 from menu import menu
 from resources import resources
 
-print(menu, resources)
+
+def print_resources(money):
+    print(
+        f"Water: {resources['water']}ml\nMilk: {resources['milk']}ml\nCoffee: {resources['coffee']}g\nMoney: ${money}"
+    )
+
+
+def coffee_machine():
+    machine_continue = True
+    money = 0
+    while machine_continue:
+        command = input("What would you like? (espresso/latte/cappuccino): ")
+        if command == "report":
+            print_resources(money)
+        elif command == "off":
+            machine_continue = False
+
+
+coffee_machine()
