4645004e2d54b2e87f6acd75e664d5f282315795
Tareq <tareqnmd@gmail.com>
2023-07-31 11:43:10 +0600

    turtle graphics updated

--- Day 18/turtle_square.py
+++ Day 18/turtle_square.py
@@ -1,23 +1,28 @@
 from turtle import Screen, Turtle
 
-turtle_item = Turtle()
-turtle_item.color("red")
-turtle_item.shape("turtle")
+# turtle_item = Turtle()
+# turtle_item.color("red")
+# turtle_item.shape("turtle")
+# for _ in range(4):
+#     turtle_item.forward(100)
+#     turtle_item.left(90)
+# for _ in range(15):
+#     turtle_item.forward(10)
+#     turtle_item.penup()
+#     turtle_item.forward(10)
+#     turtle_item.pendown()
 
-turtle_item.forward(100)
-turtle_item.left(90)
-turtle_item.forward(100)
-turtle_item.left(90)
-turtle_item.forward(100)
-turtle_item.left(90)
-turtle_item.forward(100)
-turtle_item.right(90)
+turtle_new_item = Turtle()
 
-for _ in range(15):
-    turtle_item.forward(10)
-    turtle_item.penup()
-    turtle_item.forward(10)
-    turtle_item.pendown()
+num_sides = 3
+while num_sides < 10:
+    angle = 360 / num_sides
+    print(angle)
+    for _ in range(num_sides):
+        turtle_new_item.forward(100)
+        turtle_new_item.right(angle)
+    num_sides += 1
+
 
 screen = Screen()
 screen.exitonclick()
