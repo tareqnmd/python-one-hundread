4f54db858fc75dc72aa4a7f82451f0ec1518b096
Tareq <tareqnmd@gmail.com>
2023-07-31 10:28:06 +0600

    update

--- Day 18/turtle_square.py
+++ Day 18/turtle_square.py
@@ -11,7 +11,13 @@ turtle_item.left(90)
 turtle_item.forward(100)
 turtle_item.left(90)
 turtle_item.forward(100)
-turtle_item.left(90)
+turtle_item.right(90)
+
+for _ in range(15):
+    turtle_item.forward(10)
+    turtle_item.penup()
+    turtle_item.forward(10)
+    turtle_item.pendown()
 
 screen = Screen()
 screen.exitonclick()
