7f680edd7cd690c3f64473080060fc6dedca81a3
Tareq <tareqnmd@gmail.com>
2023-07-31 14:01:43 +0600

    color mode

--- Day 18/turtle_square.py
+++ Day 18/turtle_square.py
@@ -1,5 +1,5 @@
 import random
-from turtle import Screen, Turtle
+from turtle import Screen, Turtle, colormode
 
 colors = [
     "CornflowerBlue",
@@ -37,11 +37,22 @@ directions = [0, 90, 180, 270, 360]
 #         turtle_new_item.right(angle)
 #     num_sides += 1
 
+
+colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+
 turtle_rand_item = Turtle()
 turtle_rand_item.pensize(10)
 turtle_rand_item.speed("fastest")
 for _ in range(200):
-    turtle_rand_item.color(random.choice(colors))
+    turtle_rand_item.color(random_color())
     turtle_rand_item.forward(30)
     turtle_rand_item.setheading(random.choice(directions))
 
