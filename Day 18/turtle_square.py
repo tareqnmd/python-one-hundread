c6db7760ec1b09cd1816a54ab0bebf9d6657f7af
Tareq <tareqnmd@gmail.com>
2023-07-31 10:19:57 +0600

    update turtle

---
+++ Day 18/turtle_square.py
@@ -0,0 +1,17 @@
+from turtle import Screen, Turtle
+
+turtle_item = Turtle()
+turtle_item.color("red")
+turtle_item.shape("turtle")
+
+turtle_item.forward(100)
+turtle_item.left(90)
+turtle_item.forward(100)
+turtle_item.left(90)
+turtle_item.forward(100)
+turtle_item.left(90)
+turtle_item.forward(100)
+turtle_item.left(90)
+
+screen = Screen()
+screen.exitonclick()
