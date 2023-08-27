2b1fef18a4a05d77c3a82f8f837411be51e9b1df
Tareq <tareqnmd@gmail.com>
2023-08-27 18:07:17 +0600

    25 updated

--- Day 25/weather.py
+++ Day 25/weather.py
@@ -1,4 +1,5 @@
 weather_file_path = "./Python 1-100/Day 25/weather_data.csv"
+student_score_file_path = "./Python 1-100/Day 25/student_score.csv"
 
 # with open(weather_file_path) as weather_file:
 #     data = weather_file.readlines()
@@ -20,4 +21,27 @@ import pandas
 
 data = pandas.read_csv(weather_file_path)
 print(data)
-print(data["temp"])
+print(data.to_dict())
+print(data[data.day == "Tuesday"])
+print(data[data.temp == data.temp.max()])
+
+temp = data["temp"]
+print(temp)
+temp_list = temp.to_list()
+avg_temp = sum(temp_list) / len(temp_list)
+print(temp_list, avg_temp)
+temp_mean = temp.mean()
+temp_max = temp.max()
+print(temp_mean, temp_max)
+
+condition = data.condition
+print(condition)
+monday = data[data.day == "Monday"]
+monday_temp = monday.temp
+print(monday)
+print(monday_temp)
+
+
+data_dict = {"students": ["aaa", "bbb", "ccc"], "scores": [40, 44, 38]}
+data = pandas.DataFrame(data_dict)
+data.to_csv(student_score_file_path)
