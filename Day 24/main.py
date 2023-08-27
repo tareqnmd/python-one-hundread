68b68beadd33576b5a133dab01fcbc83fe40a8cb
Tareq <tareqnmd@gmail.com>
2023-08-27 13:40:20 +0600

    24 completed

--- Day 24/main.py
+++ Day 24/main.py
@@ -1,16 +1,16 @@
 PLACEHOLDER = "[name]"
 
 
-with open("../Python 1-100/Day 24/Input/Names/invited_names.txt") as names_file:
+with open("./Python 1-100/Day 24/Input/Names/invited_names.txt") as names_file:
     names = names_file.readlines()
 
-with open("../Python 1-100/Day 24/Input/Letters/starting_letter.txt") as letter_file:
+with open("./Python 1-100/Day 24/Input/Letters/starting_letter.txt") as letter_file:
     letter_contents = letter_file.read()
     for name in names:
         stripped_name = name.strip()
         new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
         with open(
-            f"../Python 1-100/Day 24/Output/ReadyToSend/letter_for_{stripped_name}.txt",
+            f"./Python 1-100/Day 24/Output/ReadyToSend/letter_for_{stripped_name}.txt",
             mode="w",
         ) as completed_letter:
             completed_letter.write(new_letter)
