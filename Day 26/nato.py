cdbd11235533dab7a8467904d8f4aa6d7469c04e
Tareq <tareqnmd@gmail.com>
2023-08-28 21:31:25 +0600

    nato updated

--- Day 26/nato.py
+++ Day 26/nato.py
@@ -4,6 +4,7 @@ data = pandas.read_csv("./Python 1-100/Day 26/nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
+print("\n")
 
 word = input("Enter a word: ").upper()
 output_list = [phonetic_dict[letter] for letter in word]
